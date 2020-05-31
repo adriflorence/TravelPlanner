59a6880fa6eecb93af0d11022bfc61c783776a13
Adri Forczek <forczekadri@gmail.com>
2020-05-31 23:56:50 +0100

    chore: saves search to localStorage and loads it back to front-end

--- src/client/js/saveTrip.js
+++ src/client/js/saveTrip.js
@@ -1,7 +1,18 @@
 function saveTrip() {
     // get trip details that belong to the button
+    let city = document.getElementById('city').value
+    let start_date = document.getElementById('start_date').value
+    let end_date = document.getElementById('end_date').value
 
     // save them to localStorage
+    window.localStorage.setItem('city', city);
+    window.localStorage.setItem('start_date', start_date);
+    window.localStorage.setItem('end_date', end_date);
 
     // display saved entry on front-end
+    let saved_trip = document.getElementById('saved_trips');
+    let ls_city = window.localStorage.getItem('city');
+    saved_trip.innerText = ls_city;
 }
+
+export { saveTrip }
